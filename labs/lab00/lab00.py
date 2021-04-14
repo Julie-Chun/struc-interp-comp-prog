ecd7ed3373d595e724866b7d2749f4a3c1f6124e
Julie-Chun <69527918+Julie-Chun@users.noreply.github.com>
2021-04-14 00:48:14 -0700

    Add files via upload

--- labs/lab00/lab00.py
+++ labs/lab00/lab00.py
@@ -1,1 +1,8 @@
+def twenty_twenty():
+    """Come up with the most creative expression that evaluates to 2019,
+    using only numbers and the +, *, and - operators.
 
+    >>> twenty_twenty()
+    2020
+    """
+    return 11+984*734-3352-528*1145+46*12-999*113
