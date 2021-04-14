770485b1e95679c64fffc4ebde4e3305eae1b687
Julie-Chun <69527918+Julie-Chun@users.noreply.github.com>
2021-04-14 01:05:17 -0700

    Add files via upload

---
+++ labs/lab06/lab06.py
@@ -0,0 +1,120 @@
+this_file = 'lab06.py'
+def make_adder_inc(n):
+    """
+    >>> adder1 = make_adder_inc(5)
+    >>> adder2 = make_adder_inc(6)
+    >>> adder1(2) 
+    7
+    >>> adder1(2) # 5 + 2 + 1
+    8
+    >>> adder1(10) # 5 + 10 + 2
+    17
+    >>> [adder1(x) for x in [1, 2, 3]]
+    [9, 11, 13]
+    >>> adder2(5)
+    11
+    """
+    counter = -1
+    def adder(input):
+        nonlocal n
+        nonlocal counter
+        counter += 1
+        return n + input + counter
+
+    return adder
+
+
+def make_fib():
+    """Returns a function that returns the next Fibonacci number
+    every time it is called.
+
+    >>> fib = make_fib()
+    >>> fib()
+    0
+    >>> fib()
+    1
+    >>> fib()
+    1
+    >>> fib()
+    2
+    >>> fib()
+    3
+    >>> fib2 = make_fib()
+    >>> fib() + sum([fib2() for _ in range(5)])
+    12
+    >>> from construct_check import check
+    >>> # Do not use lists in your implementation
+    >>> check(this_file, 'make_fib', ['List'])
+    True
+    """
+    current, nxt = 0,1
+    def fib():
+        nonlocal nxt
+        nonlocal current
+
+        prev = current
+        current = nxt
+        nxt = prev + current
+        return prev
+    return fib
+
+
+# Generators
+def naturals():
+    """A generator function that yields the infinite sequence of natural
+    numbers, starting at 1.
+
+    >>> m = naturals()
+    >>> type(m)
+    <class 'generator'>
+    >>> [next(m) for _ in range(10)]
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    """
+    i = 1
+    while True:
+        yield i
+        i += 1
+
+def scale(it, multiplier):
+    """Yield elements of the iterable it scaled by a number multiplier.
+
+    >>> m = scale([1, 5, 2], 5)
+    >>> type(m)
+    <class 'generator'>
+    >>> list(m)
+    [5, 25, 10]
+
+    >>> m = scale(naturals(), 2)
+    >>> [next(m) for _ in range(5)]
+    [2, 4, 6, 8, 10]
+    """
+    copy = iter(it)
+    for i in copy:
+        yield i*multiplier
+        
+    
+
+def hailstone(n):
+    """
+    >>> for num in hailstone(10):
+    ...     print(num)
+    ...
+    10
+    5
+    16
+    8
+    4
+    2
+    1
+    """
+    num = n
+    while num != 1:
+        yield num
+        if num%2 ==0:
+            num = num//2
+        else:
+            num = num*3 +1
+
+    yield 1
+
+
